495f85f17b1dd591e8d0b6538cd05c8ae4c24776
soomnia <soomnia95@gmail.com>
2024-04-19 14:48:56 +0900

    feat: Rename remove_odd -> split_odd_even
    not just removing odds, assign odds and evens to each list and return them in a tupple

--- main.py
+++ main.py
@@ -2,12 +2,20 @@
 def check_available():
     return "it works :)"
 
-def remove_odd(nums=[1,2,3,4]):
-    return [ num for num in nums if num % 2 == 0 ]
+def split_odd_even(nums=[1,2,3,4]):
+    odds = []
+    evens = []
+    for num in nums:
+        if num % 2 == 0:
+            evens.append(num)
+        else:
+            odds.append(num)
+    return (odds, evens)
 
 
 if __name__ == '__main__':
     # print(check_available())
-    print(remove_odd())
+    # print(remove_odd())
+    print(split_odd_even())
 
 
