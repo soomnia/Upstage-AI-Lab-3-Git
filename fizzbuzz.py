6baefe3a56d1ec0207745192a6d728f598a672a4
soomnia <soomnia95@gmail.com>
2024-04-22 12:10:37 +0900

    fix: Modified fizzbuzz
    modified to operate as its origin purpose
    On branch [fb-refactor]

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 for i in range(1, 16):
-    if i % 3 == 0:
-        print("fizz")
+    if i % 15 == 0:
+        print("fizzbuzz")
     elif i % 5 == 0:
         print("buzz")
+    elif i % 3 == 0:
+        print("fizz")
     else:
         print(i)
